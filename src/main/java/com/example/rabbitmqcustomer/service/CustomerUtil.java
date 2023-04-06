42106c7757cd9cb2874d3cd26501d4b3b745be20
黄岩 <huangyan123>
2023-04-06 23:38:40 +0800

    customer

---
+++ src/main/java/com/example/rabbitmqcustomer/service/CustomerUtil.java
@@ -0,0 +1,37 @@
+package com.example.rabbitmqcustomer.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+//@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class CustomerUtil {
+
+    @RabbitHandler
+    @RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者1收到消息  : " + testMessage.toString());
+    }
+
+    @RabbitHandler
+    @RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+    public void process2(Map testMessage) {
+        System.out.println("DirectReceiver消费者2收到消息  : " + testMessage.toString());
+    }
+
+    @RabbitHandler
+    @RabbitListener(queues = "topic.man")//监听的队列名称 topic.man
+    public void manListener(Map testMessage) {
+        System.out.println("topic.man收到消息  : " + testMessage.toString());
+    }
+
+    @RabbitHandler
+    @RabbitListener(queues = "topic.woman")//监听的队列名称 topic.woman
+    public void womanListener(Map testMessage) {
+        System.out.println("topic.woman收到消息  : " + testMessage.toString());
+    }
+
+}
---
+++ src/test/java/com/example/rabbitmqcustomer/RabbitmqCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
