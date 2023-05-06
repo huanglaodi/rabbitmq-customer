c17ff83fedc12ddc4bef6f5c81abf0d2e7d67e63
黄岩 <huangyan123>
2023-05-06 12:25:55 +0800

    设置成cloud服务

--- src/main/java/com/example/rabbitmqcustomer/RabbitmqCustomerApplication.java
+++ src/main/java/com/example/rabbitmqcustomer/RabbitmqCustomerApplication.java
@@ -3,9 +3,11 @@ package com.example.rabbitmqcustomer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.rabbitmqcustomer")
+@EnableDiscoveryClient
 public class RabbitmqCustomerApplication {
 
     public static void main(String[] args) {
