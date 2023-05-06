d7a4f056d0ee32c9f57c104d028220ef2e25ad0a
黄岩 <huangyan123>
2023-05-07 00:57:15 +0800

    设置成cloud服务

--- src/main/java/com/example/rabbitmqcustomer/RabbitmqCustomerApplication.java
+++ src/main/java/com/example/rabbitmqcustomer/RabbitmqCustomerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan("com.example.rabbitmqcustomer")
 @EnableDiscoveryClient
 public class RabbitmqCustomerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqCustomerApplication.class, args);
     }
